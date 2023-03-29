ffc523933ccb5c7c509df30e1d312bb59591ae35
Chinmai3 <123291366+Chinmai3@users.noreply.github.com>
2023-03-29 10:53:20 +0530

    Create output_format.java

---
+++ output_format.java
@@ -0,0 +1,9 @@
+2
+tea
+50
+100
+coffee
+100
+50
+[[tea,50,100],[coffee,100,50]]      //[[productname,production rate in 1st year,production rate in 2nd year].........] 
+[[50,200.0],[-50,-200.0]]           //[[variance in production rate in two years  ,   variance % with respect to the value of variance and initial production rate]......]
